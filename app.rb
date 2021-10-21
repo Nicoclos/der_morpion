9dc96be966a369f62fcd4e0af31fcb46ee7a4a03
nicoclos <n.bia@outlook.fr>
2021-10-21 11:02:31 +0200

    initialisation.1.2

--- app.rb
+++ app.rb
@@ -2,3 +2,18 @@ require 'bundler'
 Bundler.require
 
 require_relative 'lib/morpion'
+
+def welcome
+    puts ("----------------------------------------------")
+    puts ("|    Bienvenue pour une partie de MORPION    |")
+    puts ("| Votre objectif : vaincre votre adversaire! |")
+    puts ("| Comment ? En alignant 3 symboles ! Forza ! |")
+    puts ("----------------------------------------------")
+    gets.chomp
+end
+
+
+#Initialisation joueur
+puts ("Comment dois-je vous appeler ?")
+pseudo = gets.chomp
+user = HumanPlayer.new(pseudo)
